4627fe411d8c8391905cc71c1ce69ca446b8a3ec
SAJID LATIF <indus1530@gmail.com>
2020-10-14 16:40:01 +0500

    SectionF01Activity added

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_tmk_el/ui/sections/SectionF01Activity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.uen_tmk_el.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.uen_tmk_el.R;
+
+public class SectionF01Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_f01);
+    }
+}
