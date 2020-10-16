6d45c99b1e337eb906b6c88999ef99c073a480c8
SAJID LATIF <indus1530@gmail.com>
2020-10-16 15:19:58 +0500

    SectionBActivity added and Strings replaced

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_tmk_el/ui/sections/SectionBActivity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.uen_tmk_el.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.uen_tmk_el.R;
+
+public class SectionBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_b);
+    }
+}
