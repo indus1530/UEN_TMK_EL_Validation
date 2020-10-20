78177127d25f64f719656ef6b8b382ba515cb851
muhammad.farooqui <waqarfarooqui.12@gmail.com>
2020-10-20 18:28:38 +0500

    Fixed sectionF divided on F06 sections and merge.

--- app/src/main/java/edu/aku/hassannaqvi/uen_tmk_el/ui/sections/SectionF06Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_tmk_el/ui/sections/SectionF06Activity.java
@@ -1,6 +1,5 @@
 package edu.aku.hassannaqvi.uen_tmk_el.ui.sections;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,32 +8,32 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import com.validatorcrawler.aliazaz.Clear;
 import com.validatorcrawler.aliazaz.Validator;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import edu.aku.hassannaqvi.uen_tmk_el.R;
-import edu.aku.hassannaqvi.uen_tmk_el.core.MainApp;
-import edu.aku.hassannaqvi.uen_tmk_el.databinding.ActivitySectionF01Binding;
+import edu.aku.hassannaqvi.uen_tmk_el.databinding.ActivitySectionF06Binding;
 import edu.aku.hassannaqvi.uen_tmk_el.ui.other.MainActivity;
 import edu.aku.hassannaqvi.uen_tmk_el.utils.AppUtilsKt;
 
-public class SectionF01Activity extends AppCompatActivity {
+public class SectionF06Activity extends AppCompatActivity {
 
-    ActivitySectionF01Binding bi;
-    int raf = 0;
+    ActivitySectionF06Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_f01);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_f06);
         bi.setCallback(this);
         setupSkip();
     }
 
 
     private void setupSkip() {
+        bi.cmf10.setOnCheckedChangeListener((group, id) -> Clear.clearAllFields(bi.llcmf11t12));
     }
 
 
@@ -75,48 +74,40 @@ public class SectionF01Activity extends AppCompatActivity {
 
         JSONObject json = new JSONObject();
 
-        json.put("f1a", bi.f1a.getText().toString());
-        json.put("f1b", bi.f1b.getText().toString());
-
-        json.put("raf1", bi.raf101.isChecked() ? "1"
-                : bi.raf102.isChecked() ? "2"
+        json.put("cmf10", bi.cmf1001.isChecked() ? "1"
+                : bi.cmf1002.isChecked() ? "2"
+                : bi.cmf1003.isChecked() ? "66"
+                : bi.cmf1098.isChecked() ? "98"
                 : "-1");
 
-        json.put("raf2", bi.raf2.getText().toString());
-
-        json.put("raf301", bi.raf301.getText().toString());
-        json.put("raf302", bi.raf302.getText().toString());
-        json.put("raf303", bi.raf303.getText().toString());
-        json.put("raf304", bi.raf304.getText().toString());
-
-        json.put("raf4", bi.raf401.isChecked() ? "1"
-                : bi.raf402.isChecked() ? "2"
-                : bi.raf498.isChecked() ? "98"
+        json.put("cmf11", bi.cmf1101.isChecked() ? "1"
+                : bi.cmf1102.isChecked() ? "2"
+                : bi.cmf1103.isChecked() ? "3"
+                : bi.cmf1104.isChecked() ? "4"
+                : bi.cmf1196.isChecked() ? "96"
                 : "-1");
+        json.put("cmf1196x", bi.cmf1196x.getText().toString());
 
-        json.put("raf5", bi.raf5.getText().toString());
+        json.put("cmf1201", bi.cmf1201.isChecked() ? "1" : "-1");
+        json.put("cmf1202", bi.cmf1202.isChecked() ? "2" : "-1");
+        json.put("cmf1203", bi.cmf1203.isChecked() ? "3" : "-1");
+        json.put("cmf1204", bi.cmf1204.isChecked() ? "4" : "-1");
+        json.put("cmf1205", bi.cmf1205.isChecked() ? "5" : "-1");
+        json.put("cmf1206", bi.cmf1206.isChecked() ? "6" : "-1");
 
-        MainApp.form.setsF(json.toString());
+       /* try {
+            JSONObject json_merge = JSONUtils.mergeJSONObjects(new JSONObject(death.getsF()), json);
+
+            death.setsF(String.valueOf(json_merge));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }*/
 
     }
 
 
     private boolean formValidation() {
-
-        if (!bi.raf301.getText().toString().trim().isEmpty()
-                && !bi.raf302.getText().toString().trim().isEmpty()
-                && !bi.raf303.getText().toString().trim().isEmpty()
-                && !bi.raf304.getText().toString().trim().isEmpty()) {
-
-            if ((Integer.parseInt(bi.raf301.getText().toString())
-                    + Integer.parseInt(bi.raf302.getText().toString())
-                    + Integer.parseInt(bi.raf303.getText().toString())
-                    + Integer.parseInt(bi.raf304.getText().toString())) > 15) {
-                Toast.makeText(this, "Question RAF3 \nAll Pregnancies Can't be greater than 15!", Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        }
-
         return Validator.emptyCheckingContainer(this, bi.GrpName);
     }
 
